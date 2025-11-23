c9f408fdf776be0b2daf9b68b373d26cc2bd3758
Murasaki55 <155960585+Murasaki55@users.noreply.github.com>
2025-11-23 19:02:27 +0100

    update revision ans footer

---
+++ Competences/globalpotfolio.js
@@ -0,0 +1,21 @@
+imagesfull = document.querySelectorAll(".full")
+images = document.querySelectorAll(".image")
+div = document.querySelector("#divfull")
+
+images.forEach(img => {
+    img.addEventListener('click',() => {
+        imagesfull.forEach(imgf => {
+            if (img.src == imgf.src){
+                div.style.display = "block"
+                imgf.style.display = "inline"
+            }
+        });
+    })
+});
+
+div.addEventListener("click", () => {
+    imagesfull.forEach(imgf => {
+        imgf.style.display = "none"
+        div.style.display = "none"
+    })
+})
